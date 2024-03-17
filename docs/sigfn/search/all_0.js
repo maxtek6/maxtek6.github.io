76771bb40541e1479d225a75246a5f8d738cea63
maxtek6 <johnpatek2@gmail.com>
2024-03-17 02:14:14 +0000

    Update from https://github.com/maxtek6/sigfn/commit/d10c2731f32eae8030bd70e10abc4da90bbef3e7

--- docs/sigfn/search/all_0.js
+++ docs/sigfn/search/all_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['c_0',['C',['../md_README.html#autotoc_md3',1,'C'],['../md_README.html#autotoc_md4',1,'C++']]]
+  ['c_0',['C',['../md_README.html#autotoc_md3',1,'C'],['../index.html#autotoc_md7',1,'C'],['../md_README.html#autotoc_md4',1,'C++'],['../index.html#autotoc_md8',1,'C++']]]
 ];
--- docs/sigfn/search/all_7.js
+++ docs/sigfn/search/all_7.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['usage_0',['Usage',['../md_README.html#autotoc_md2',1,'']]]
+  ['usage_0',['Usage',['../md_README.html#autotoc_md2',1,'Usage'],['../index.html#autotoc_md6',1,'Usage']]]
 ];
